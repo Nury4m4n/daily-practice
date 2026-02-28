86836d551907f1cc3425cc7f12870522c14f0001
nury4m4n <nury4m4n13@gmail.com>
2026-02-28 23:57:45 +0700

    feat: solve multiply

---
+++ JavaScript/multiply.js
@@ -0,0 +1,12 @@
+/*
+Description:
+This code does not execute properly. Try to figure out why.
+
+Codewars Kata:
+    https://www.codewars.com/kata/50654ddff44f800200000004
+*/
+
+const multiply = (a, b) => a * b;
+
+// Example
+console.log(multiply(2, 1)); // Output: "2"
