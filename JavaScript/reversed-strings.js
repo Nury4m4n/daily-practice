fbee6060814b3734bc57eb4475a214a7c7c8ee5f
nury4m4n <nury4m4n13@gmail.com>
2026-03-01 16:31:50 +0700

    feat: solve reversed strings

---
+++ JavaScript/reversed-strings.js
@@ -0,0 +1,11 @@
+/*
+Instructions:
+Complete the solution so that it reverses the string passed into it.
+
+Codewars Kata:
+   https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+*/
+
+const solution = str => str.split('').reverse().join('')
+
+console.log(solution("zee")) // Output: "eez"
