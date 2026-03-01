31fc1bd5e6ff459e69a0fd7b29d03f2876976bbd
nury4m4n <nury4m4n13@gmail.com>
2026-03-01 16:41:47 +0700

    feat: solve convert a number to a string

---
+++ JavaScript/convert-a-number-to-a-string.js
@@ -0,0 +1,13 @@
+/*
+Instructions:
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?ers.
+
+Codewars Kata:
+    https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+*/
+
+const numberToString = num => num.toString();
+
+// Example
+console.log(numberToString(123))
