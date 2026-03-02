ce4a8e60588a0a641d13e6513c217c0e074c9503
nury4m4n <nury4m4n13@gmail.com>
2026-03-03 03:15:51 +0700

    feat: solve opposite number

---
+++ JavaScript/opposite-number.js
@@ -0,0 +1,12 @@
+/*
+Instructions:
+Very simple, given a number, find its opposite (additive inverse).
+
+Codewars Kata:
+    https://www.codewars.com/kata/56dec885c54a926dcd001095
+*/
+
+const opposite = number => -number;
+
+// Example
+console.log(opposite(123)) // Output: "-123"
