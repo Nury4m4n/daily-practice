d99770958b49f4ba006ee12f2a60876bdba34cf3
nury4m4n <nury4m4n13@gmail.com>
2026-03-07 04:46:43 +0700

    feat: solve century from year

---
+++ JavaScript/century-from-year.js
@@ -0,0 +1,11 @@
+/*
+Instructions:
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Codewars Kata:
+   https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+*/
+
+const century = (year) => Math.ceil(year / 100);
+
+console.log(century(1759)); // Output: "20"
